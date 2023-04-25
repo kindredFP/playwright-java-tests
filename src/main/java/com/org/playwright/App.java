547463236c2bf66f77d72c3b8cc35d099233e0a7
Francis <kindredfp@users.noreply.github.com>
2023-04-24 22:51:15 -0400

    first commit

---
+++ src/main/java/com/org/playwright/App.java
@@ -0,0 +1,32 @@
+package com.org.playwright;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+
+import java.util.regex.Pattern;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class App {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch();
+            BrowserContext context = browser.newContext();
+            Page page = context.newPage();
+            page.navigate("http://playwright.dev");
+            System.out.println(page.title());
+
+            // create a locator
+            Locator getStarted = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Get Started"));
+
+            // Expect an attribute "to be strictly equal" to the value.
+            assertThat(getStarted).hasAttribute("href", "/docs/intro");
+
+            // Click the get started link.
+            getStarted.click();
+
+            // Expects the URL to contain intro.
+            assertThat(page).hasURL(Pattern.compile(".*intro"));
+        }
+    }
+}
